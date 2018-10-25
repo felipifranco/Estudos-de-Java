67ea6f254a451babbc5a81c776bc406bd503dfc5
felipifranco <felipimfranco@gmail.com>
2018-10-25 17:44:43 -0300

    Semana 1

---
+++ Semana1/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+    public static void main(String[] args) { 
+
+        System.out.printf("Primeira linha\n");
+        System.out.printf("Segunda linha");
+
+    } 
+
+}
---
+++ Soma.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Soma {
+
+    public static void main(String[] args) { 
+
+        Scanner input = new Scanner(System.in);
+
+        int num1;
+        int num2;
+        int sum;
+
+        System.out.printf("Primeiro numero:");
+        num1 = input.nextInt();
+        
+        System.out.printf("Segundo numero:");
+        num2 = input.nextInt();
+
+        sum = num1 + num2;
+
+        System.out.print(sum);
+
+        input.close();
+        
+    } 
+
+}
