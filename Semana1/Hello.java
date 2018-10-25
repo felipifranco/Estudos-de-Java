605f01c3e2b47f20643949fdef5c12bf52514045
felipifranco <felipimfranco@gmail.com>
2018-10-25 20:59:29 -0300

    Semana 1

--- Semana1/Hello.java
+++ Semana1/Hello.java
@@ -1,11 +1,27 @@
-
 public class Hello {
 
     public static void main(String[] args) { 
 
-        System.out.printf("Primeira linha\n");
+        System.out.println("Primeira linha");
+        System.out.println("Segunda linha");
+
+        System.out.print("------------------\n");
+
+        System.out.print("Primeira linha");
+        System.out.print("Segunda linha");
+
+        System.out.print("------------------\n");
+
+        System.out.printf("Primeira linha");
         System.out.printf("Segunda linha");
 
     } 
 
 }
+/*
+Primeira linha
+Segunda linha
+------------------
+Primeira linhaSegunda linha------------------
+Primeira linhaSegunda linha
+*/
