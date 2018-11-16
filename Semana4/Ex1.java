88825df8863479dceff9e27363ac6e53a3aaae15
felipifranco <felipimfranco@gmail.com>
2018-11-16 19:37:56 -0200

    Semana 4

--- Semana4/Ex1.java
+++ Semana4/Ex1.java
@@ -1,9 +1,37 @@
+// Pergunta 1:
+// Deseja-se implementar um método criaVec(int tam, int val) que crie um vetor vec de tamanho
+// tam e o inicialize com os valores {val, 2*val, 3*val, ... }.
+
+//Pergunta 2:
+// Assinale a alternativa que contém uma implementação possível do método main que
+// execute criaVec com parâmetros tam = 10 e val = 3, e então imprima os valores do vetor
+// resultante:
+
 /**
  * Ex1
  */
 public class Ex1 {
+    
+    static int[] criaVec(int tam, int val){
+        int vec[] = new int[tam];
+        // for (int i = 0; i < tam ; i++){
+        for (int i = 0; i < vec.length ; i++){
+            vec[i] = (i+1)*val;
+        }
+        return vec;
+    }
 
     public static void main(String[] args) {
         
+        int[] vec = criaVec(10, 3);
+
+        // for ( int i = 0 ; i < vec.length ; i++ ){
+        //     System.out.print(vec[i] + " ");
+        // }
+
+        for( int i : vec){
+            System.out.print(i + " ");
+        }
+
     }
 }
---
+++ Semana4/Ex4.java
@@ -0,0 +1,8 @@
+// Pergunta 4:
+// Deseja-se implementar um método ehCrescente(int[] vec) que diga se os elementos do vetor
+// vec passados como parâmetro estão em ordem crescente ou não. Por exemplo, os vetores {1,
+// 3, 3, 4, 6, 7} e {1, 1, 3, 9, 10} estão em ordem crescente, já {1, 2, 1, 2} e {1, 1, 3, 2, 10} não
+// estão em ordem crescente. Assinale a alternativa que contém uma implementação correta
+// desse método:
+
+
