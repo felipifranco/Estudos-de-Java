a2e56024dff185650a3126231544c7de7387c742
felipifranco <felipimfranco@gmail.com>
2018-11-20 01:20:23 -0200

    Semana 4

--- Semana4/Ex5.java
+++ Semana4/Ex5.java
@@ -2,7 +2,12 @@
 // Deseja-se implementar um método subString (char[] frase, int ini, int fim) que retorne a parte
 // de frase entre as posições ini e fim (inclusive) na forma de um arranjo de caracteres. Por
 // exemplo, se frase = {‘T’, ‘e’, ‘s’, ‘t’, ‘a’, ‘n’, ‘d’, ‘o’}, uma chamada a subString(frase, 0, 3) deve
-// retornar {‘T’, ‘e’, ‘s’, ‘t’}, enquanto uma chamada a subString(frase, 1, 4) deve retornar {‘e’, ‘s’,
-// ‘t’, ‘a’}. Assinale a alternativa que contém uma implementação correta desse método. Assuma
+// retornar {‘T’, ‘e’, ‘s’, ‘t’}, enquanto uma chamada a subString(frase, 1, 4) deve 
+// retornar {‘e’, ‘s’, ‘t’, ‘a’}. Assinale a alternativa que contém uma implementação correta desse método. Assuma
 // que ini e fim são entradas válidas, ou seja, não é necessário testar por valores negativos ou
 // maiores do que o tamanho da frase.
+
+//                   0    1    2    3    4    5    6    7   (portanto, n+1)
+//                                                             8 posiçoes 
+// char[] frase =  {'T', 'e', 's', 't', 'a', 'n', 'd', 'o'};
+
