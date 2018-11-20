73b8d5a760a5f1c28b15120370a13a95f97045a6
felipifranco <felipimfranco@gmail.com>
2018-11-19 23:28:06 -0200

    Semana 4

--- Semana4/Ex4.java
+++ Semana4/Ex4.java
@@ -5,4 +5,26 @@
 // estão em ordem crescente. Assinale a alternativa que contém uma implementação correta
 // desse método:
 
+//int[] vector = {1, 3, 3, 4, 6, 7};
 
+/**
+ * Ex4
+ */
+public class Ex4 {
+
+    static boolean ehCrescente(int[] vec){
+
+        for(int i=0; i < vec.length-1; i++){
+            if( vec[i+1] < vec[i] ){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] vector = {1, 1, 3, 9, 10};
+
+        System.out.println(ehCrescente(vector)); 
+    }
+}
---
+++ Semana4/Ex5.java
@@ -0,0 +1,8 @@
+// Pergunta 5:
+// Deseja-se implementar um método subString (char[] frase, int ini, int fim) que retorne a parte
+// de frase entre as posições ini e fim (inclusive) na forma de um arranjo de caracteres. Por
+// exemplo, se frase = {‘T’, ‘e’, ‘s’, ‘t’, ‘a’, ‘n’, ‘d’, ‘o’}, uma chamada a subString(frase, 0, 3) deve
+// retornar {‘T’, ‘e’, ‘s’, ‘t’}, enquanto uma chamada a subString(frase, 1, 4) deve retornar {‘e’, ‘s’,
+// ‘t’, ‘a’}. Assinale a alternativa que contém uma implementação correta desse método. Assuma
+// que ini e fim são entradas válidas, ou seja, não é necessário testar por valores negativos ou
+// maiores do que o tamanho da frase.
