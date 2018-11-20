13ccf6bc740644e4e4997721b869b08ad461a08e
felipifranco <felipimfranco@gmail.com>
2018-11-20 13:13:01 -0200

    Semana 4

--- Semana4/Ex5.java
+++ Semana4/Ex5.java
@@ -6,8 +6,26 @@
 // retornar {‘e’, ‘s’, ‘t’, ‘a’}. Assinale a alternativa que contém uma implementação correta desse método. Assuma
 // que ini e fim são entradas válidas, ou seja, não é necessário testar por valores negativos ou
 // maiores do que o tamanho da frase.
-
+//                                                     (n) posições
 //                   0    1    2    3    4    5    6    7   (portanto, n+1)
-//                                                             8 posiçoes 
+//                                                             8 posições 
 // char[] frase =  {'T', 'e', 's', 't', 'a', 'n', 'd', 'o'};
 
+/**
+ * Ex5
+ */
+public class Ex5 {
+    static char[] subString(char[] frase, int ini, int fim){
+        char[] result = new char[fim-ini+1];
+
+        for(int i=0; i < result.length; i++, ini++ ){
+            result[i] = frase[ini];
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        char[] frase =  {'T', 'e', 's', 't', 'a', 'n', 'd', 'o'};
+
+        System.out.println(subString(frase, 1, 4));
+    }
+}
